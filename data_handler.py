898ea0aa4b9211bf5f1702717187afd2c3cf4873
Sardorbek Zokirov <sardorzakirov1999@gmail.com>
2022-04-25 11:23:40 +0200

    Choosing functions

--- data_handler.py
+++ data_handler.py
@@ -7,7 +7,7 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, PowerTransformer, OrdinalEncoder, LabelEncoder
 from sklearn.model_selection import train_test_split
 import time
-from sklearn.pipeline import Pipeline
+from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.metrics import accuracy_score, balanced_accuracy_score, plot_confusion_matrix
 
 
@@ -29,6 +29,7 @@ data = pd.read_csv(r'data\heart.csv')
 x = data.drop(['output'], axis=1) # features - train and val data
 y = data['output'] # target
 
+vals = ['age', 'cp', 'trtbps', 'chol', 'thalachh', 'oldpeak', 'slp', 'caa', 'thall']
 x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2, random_state=0)
 
 scaler_pipeline = Pipeline([
@@ -36,7 +37,7 @@ scaler_pipeline = Pipeline([
     ('scaler', StandardScaler())
 ])
 
-tree_pipe = ColumnTransformer(['num', scaler_pipeline, [0, -1]], remainder='passthrough')
+tree_pipe = ColumnTransformer(['num', scaler_pipeline, vals], remainder='passthrough')
 
 classifiers = {
     "Decision Tree": DecisionTreeClassifier(random_state=0),
@@ -49,4 +50,28 @@ classifiers = {
     "Catboost": CatBoostClassifier(random_state=0),
     "Logistic Regression": LogisticRegression(random_state=0)
 }
+
+classifiers = {name: make_pipeline(tree_pipe, model) for name, model in classifiers.items()}
+
+
+results = pd.DataFrame({'Model': [], "Accuracy Score": [], "Balanced Accuracy score": [], "Time": []})
+
+
+for model_name, model in classifiers.items():
+    start_time = time.time()
+
+    model.fit(x_train, y_train)
+
+    predics = model.predict(x_val)
+    total_time = time.time() - start_time
+
+
+    results = results.append({"Model": model_name,
+                            "Accuracy": accuracy_score(y_val, predics)*100,
+                            "Balanced Accuracy": balanced_accuracy_score(y_val, predics)*100,
+                            "Time": total_time}, ignore_index=True)
+
+results_ord = results.sort_values(by=['Accuracy'], ascending=False, ignore_index=True)
+
+print(results_ord)
 
