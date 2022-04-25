b1d546a0eb3cd5eb6e94ec49e4556df14ec3d29b
Sardorbek Zokirov <sardorzakirov1999@gmail.com>
2022-04-25 15:27:23 +0200

    Improved the model

--- data_handler_Sardorbek.py
+++ data_handler_Sardorbek.py
@@ -5,7 +5,7 @@ import pandas as pd
 from scipy import rand
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler, PowerTransformer, OrdinalEncoder, LabelEncoder
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 import time
 from sklearn.pipeline import Pipeline, make_pipeline
@@ -76,8 +76,8 @@ gen = data_enhance(data)
 x = data.drop(['output'], axis=1) # features - train and val data
 y = data['output'] # target
 
-num_vals = ['age', 'trtbps', 'chol', 'thalachh','oldpeak']
-cat_vals = ['sex', 'cp', 'fbs', 'restecg', 'exng', 'slp', 'caa', 'thall']
+num_vals = ['age', 'trtbps','thalachh', 'chol', 'oldpeak']
+cat_vals = ['sex', 'cp', 'exng', 'slp', 'caa', 'thall', 'restecg', 'fbs']
 x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2, random_state=0)
 
 # Add enhanced data to 20% of the orig data
@@ -102,13 +102,13 @@ tree_pipe = ColumnTransformer([('num', num_pipeline, num_vals), ('cat', cat_pipe
 # Different classifiers
 classifiers = {
     "Decision Tree": DecisionTreeClassifier(random_state=0),
-    "Random Forest": RandomForestClassifier(random_state=0),
-    "Ada Boost": AdaBoostClassifier(random_state=0),
-    "Extra Trees": ExtraTreesClassifier(random_state=0),
-    "Gradient Boosting": GradientBoostingClassifier(random_state=0),
+    "Random Forest": RandomForestClassifier(random_state=0, n_estimators=100),
+    "Ada Boost": AdaBoostClassifier(random_state=0, n_estimators=100),
+    "Extra Trees": ExtraTreesClassifier(random_state=0, n_estimators=100),
+    "Gradient Boosting": GradientBoostingClassifier(random_state=0, n_estimators=100),
     "XGBoost": XGBClassifier(),
-    "LightGBM": LGBMClassifier(random_state=0),
-    "Catboost": CatBoostClassifier(random_state=0),
+    "LightGBM": LGBMClassifier(random_state=0, n_estimators=100),
+    "Catboost": CatBoostClassifier(random_state=0, n_estimators=100),
     "Logistic Regression": LogisticRegression(random_state=0)
 }
 
@@ -136,19 +136,7 @@ for model_name, model in classifiers.items():
 
 results_order = results.sort_values(by=['Accuracy Score'], ascending=False, ignore_index=True)
 
-def get_inputs():
-    pass
-
-def predictor():
-
-    best_model = classifiers.get("Extra Trees")
-
-    best_model.fit(x_train, y_train)
-
-    test_pred = best_model.predict(inps)
-    pass
-
-
+print(results_order)
 
 # STD
 """
